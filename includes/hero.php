2f4eddc530e99174155c3be616a1fad888b207d1
MFaizanRizwan <f233057@cfd.nu.edu.pk>
2026-04-19 14:21:46 +0500

    [POS-58] Refactor main headline

--- includes/hero.php
+++ includes/hero.php
@@ -1,7 +1,9 @@
     <section class="hero" id="hero">
         <div class="hero-content">
             <!-- Left Content -->
-            
+            <div class="hero-text">
+                <h1>The Last POS System You'll Ever Need</h1>
+            </div>
             <!-- Right Interactive Dashboard -->
             
         </div>
