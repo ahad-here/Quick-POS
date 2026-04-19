e29a82954db4adebda2d5c5ab54ee9453f2557a7
MFaizanRizwan <f233057@cfd.nu.edu.pk>
2026-04-19 15:26:21 +0500

    POS-77, POS-78: Add social media icons and links

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,14 @@
         <!-- Brand Section -->
         <div class="footer-brand">
             <a href="#" class="footer-logo">QuickPOS</a>
+            <!-- POS-77: Add social media icons -->
+            <!-- POS-78: Link social icons to actual pages -->
+            <div class="social-links">
+                <a href="https://facebook.com/quickpos" class="social-icon" title="Facebook">f</a>
+                <a href="https://twitter.com/quickpos" class="social-icon" title="Twitter">𝕏</a>
+                <a href="https://linkedin.com/company/quickpos" class="social-icon" title="in">in</a>
+                <a href="https://instagram.com/quickpos" class="social-icon" title="Instagram">📷</a>
+            </div>
         </div>
     </div>
 
